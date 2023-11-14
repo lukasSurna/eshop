ae77caced29bcab605e543d5366030f211c084a4
Lukas Surna <lukasurna@gmail.com>
2023-11-14 11:13:41 +0200

    email verification

--- eshop/accounts/views.py
+++ eshop/accounts/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render, redirect
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
 from . import forms, models
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
 
 def register(request):
     if request.method == 'POST':
@@ -23,8 +30,20 @@ def register(request):
             )
             user.phone_number = phone_number
             user.save()
-            messages.success(request, 'Registration successful')
-            return redirect('register')
+            
+            #activation https://medium.com/@frfahim/django-registration-with-confirmation-email-bb5da011e4ef
+            current_site = get_current_site(request)
+            mail_subject = 'Activate your account'
+            message = render_to_string('accounts/account_verification_email.html', {
+                'user': user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
+            return redirect('accounts/login/?command=verification&email='+email)
     else:           
         form = forms.RegistrationForm()
     context = {
@@ -46,10 +65,26 @@ def login(request):
         else:
             messages.error(request, "Username or password is invalid.")
             return redirect('login')
-    return render(request, 'accounts/login.html')
+    return render(request, 'login.html')
 
 @login_required(login_url = 'login')
 def logout(request):
     auth.logout(request)
     messages.success(request, "You are logged out")
     return redirect('login')
+
+def activate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = models.Account._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, models.Account.DoesNotExist):
+        user = None
+        
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, "Your Account is Activated!")
+        return redirect('login')
+    else:
+        messages.error(request, "Invalid activation link")
+        return redirect('register')
--- eshop/eshop/settings.py
+++ eshop/eshop/settings.py
@@ -140,3 +140,12 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
     50: "critical",
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'testemailbjrs@gmail.com'
+EMAIL_HOST_PASSWORD = 'zqgc ejwu puxg wnkk'  #using google APP password
+EMAIL_USE_TLS = True
+#Enabling TLS is important for securing the communication between your Django application and the email server, 
+# especially when sending sensitive information, such as account activation links or password reset emails.
